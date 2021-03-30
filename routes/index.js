0b366320bb434e25f8824f6a827e3314f24c4f36
github_username <lordsnoosh@gmail.com>
2021-03-30 15:50:27 -0700

    adjustments to feature a 'landing page'

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/listings');
+  res.render('index');
 });
 
 router.get('/auth/google', passport.authenticate(
@@ -15,14 +15,14 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/listings',
-    failureRedirect : '/listings'
+    successRedirect : '/',
+    failureRedirect : '/'
   }
 ));
 
 router.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/listings');
+  res.redirect('/');
 });
 
 module.exports = router;
